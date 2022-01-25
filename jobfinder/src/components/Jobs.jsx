4a76a682b836c90809b794568c020ffe172d8265
TheJhonni <davideconi91@gmail.com>
2022-01-25 17:03:38 +0100

    hover effect

--- jobfinder/src/components/Jobs.jsx
+++ jobfinder/src/components/Jobs.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 
 function Jobs({ job }) {
   return (
-    <div className="rounded overflow-hidden shadow-md hover:shadow-xl  hover:scale-110 transform transition-all ease-out">
-      <img className="w-full" src={job.url} alt="" />
+    <div className="rounded overflow-hidden shadow-md hover:shadow-xl  hover:scale-105 transform transition-all ease-out">
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">
-          {job.title}
+          <a href={job.url} target="_blank">
+            {job.title}
+          </a>
           <Link to={"/" + job.company_name}>
             <span className="inline-block bg-gray-200 rounded-full px-3 ml-2 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-stone-600 hover:text-white">
               {job.company_name}
@@ -16,8 +17,8 @@ function Jobs({ job }) {
         </div>
         <p className="text-gray-700 text-base">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium
-          nihil.
+          quia, Nonea! Maiores et perferendis eaque, exercitatinpm startonem
+          praesentium nihil.
         </p>
       </div>
       <div className="px-6 pt-4 pb-2">
--- jobfinder/src/components/ListJobs.jsx
+++ jobfinder/src/components/ListJobs.jsx
@@ -18,22 +18,47 @@ function ListJobs() {
       console.log(error);
     }
   };
+
+  /*  const fetchCategory = async (category) => {
+    try {
+      let resp = await fetch(
+        `https://strive-jobs-api.herokuapp.com/jobs/${category}`
+      );
+      if (resp.ok) {
+        let jobs = await resp.json();
+        console.log(jobs);
+        setCategory(jobs.data);
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }; */
+
   const [jobs, setJobs] = useState([]);
   const [search, setSearch] = useState("");
+  //const [category, setCategory] = useState("");
 
   const handleSearch = (e) => {
     setSearch(e.target.value); //develop
     fetchJobs(e.target.value); // develo
   };
 
+  /* const handleSearchCategory = (e) => {
+    setCategory(e.target.value);
+    fetchCategory(e.target.value);
+  }; */
+
   useEffect(() => {
     fetchJobs();
+    //fetchCategory();
   }, []);
 
   return (
     <>
       <div className="bg-gray-200">
-        <div className="flex justify-center mb-5 bg-gray-300">
+        <div className="flex justify-evenly mb-5 bg-gray-300">
           <input
             className="rounded-full my-4 p-5"
             type="text"
@@ -41,12 +66,23 @@ function ListJobs() {
             value={search}
             onChange={handleSearch}
           />
+          {/*   <input
+            className="rounded-full my-4 p-5"
+            type="text"
+            placeholder="Filter by category"
+            value={category}
+            onChange={handleSearchCategory}
+          /> */}
         </div>
       </div>
       <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
         {jobs &&
           jobs
-            .filter((job) => true || job.title.toLowerCase().includes(search))
+            .filter(
+              (job) =>
+                true ||
+                job.title.toLowerCase().includes(search /* && category */)
+            )
             .map((job) => <Jobs key={job._id} job={job} />)}
       </div>
     </>
