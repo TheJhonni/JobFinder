d5453613f1ed2fcdcd3fc8fb1eaaf5c6c1637b6c
TheJhonni <davideconi91@gmail.com>
2022-01-26 18:01:21 +0100

    footer added

--- jobfinder/src/App.js
+++ jobfinder/src/App.js
@@ -1,19 +1,25 @@
 import Home from "./components/Home";
 import FilteredJobsByCompany from "./components/FilteredJobsByCompany";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import SavedJobsPage from "./components/SavedJobsPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1 className="bg-gray-400 text-xl py-5 text-white ">Job Finder App</h1>
+        <Link to="/">
+          <h1 className="bg-gray-400 text-xl py-5 text-white ">
+            Job Finder App
+          </h1>
+        </Link>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/:company" element={<FilteredJobsByCompany />} />
           <Route path="/favourite" element={<SavedJobsPage />} />
         </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );
